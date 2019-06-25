1b8f38484565d3794042b4922028e27937625f86
Anatolii Mihailenco <anmic.webdev@gmail.com>
2019-06-25 20:23:57 +0300

    Add client trace to context.

--- notifier.go
+++ notifier.go
@@ -48,10 +48,10 @@ func defaultHTTPClient() *http.Client {
 		httpClient = &http.Client{
 			Transport: &http.Transport{
 				Proxy: http.ProxyFromEnvironment,
-				Dial: (&net.Dialer{
+				DialContext: (&net.Dialer{
 					Timeout:   15 * time.Second,
 					KeepAlive: 30 * time.Second,
-				}).Dial,
+				}).DialContext,
 				TLSHandshakeTimeout: 10 * time.Second,
 				TLSClientConfig: &tls.Config{
 					ClientSessionCache: tls.NewLRUClientSessionCache(1024),
--- route_trace_test.go
+++ route_trace_test.go
@@ -1,6 +1,9 @@
 package gobrake
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptrace"
 	"time"
 
 	"github.com/jonboulle/clockwork"
@@ -44,5 +47,30 @@ var _ = Describe("RouteTrace", func() {
 		Expect(trace.groups["root"]).To(BeNumerically("==", 2*time.Millisecond))
 		Expect(trace.groups["nested1"]).To(BeNumerically("==", 3*time.Millisecond))
 		Expect(trace.groups["other"]).To(BeNumerically("==", 0))
+	})
+
+	It("adds client trace to request context", func() {
+		c := context.Background()
+		c, trace := NewRouteTrace(c, "GET", "https://example.com")
+
+		req, _ := http.NewRequest(trace.Method, trace.Route, nil)
+		req = req.WithContext(c)
+
+		clientTrace := &httptrace.ClientTrace{
+			ConnectDone: func(network, addr string, err error) {
+				fakeClock.Advance(time.Millisecond)
+			},
+		}
+		c = httptrace.WithClientTrace(req.Context(), clientTrace)
+		req = req.WithContext(c)
+
+		notifier := NewNotifier(1, "key")
+		for i := 0; i < 2; i++ {
+			_, err := notifier.opt.HTTPClient.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		Expect(trace.groups).To(HaveLen(1))
+		Expect(trace.groups["GET:https://example.com"]).To(Equal(2 * time.Millisecond))
 	})
 })
